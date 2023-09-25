4c7b495eb48d5208d892dbef2adb4c1df38ed874
Madsen <Chris.Madsen@gov.bc.ca>
2023-09-25 09:49:17 -0700

    use fun to get wb poly

--- R/find_downstream_waterbodies.R
+++ R/find_downstream_waterbodies.R
@@ -30,47 +30,49 @@ find_downstream_waterbodies = function(
   # Getting Polygon to Search From#
   # # # # # # # # # # # # # # # # #
 
-  # Make sure lake name is in title format.
-  if(!is.null(focus_wb_name)){
-  focus_wb_name = stringr::str_to_title(focus_wb_name)
-  }
-
-  # No coordinates?
-  if(is.null(focus_wb_coordinates)){
-    # But there is a name?
-    if(!is.null(focus_wb_name)){
-      # Look for the lake by name.
-      lake_find_attempt = bcdata::bcdc_query_geodata('cb1e3aba-d3fe-4de1-a2d4-b8b6650fb1f6') |>
-        bcdata::filter(GNIS_NAME_1 == focus_wb_name) |>
-        bcdata::collect()
-      # Is there only a single lake in BC with that name?
-      if(nrow(lake_find_attempt) == 1){
-        # If so, use that!
-        focus_wb_poly = lake_find_attempt
-      } else {
-        focus_wb_poly = lake_find_attempt |>
-          dplyr::mutate(Area = 1e-6 * as.numeric(sf::st_area(geometry))) |>
-          dplyr::arrange(desc(AREA)) |> dplyr::slice(1)
-        cat(paste0("We searched for the lake with only a name, and ", nrow(lake_find_attempt), " lakes were returned."))
-        cat("Using the largest lake from that list...")
-      }
-    }
-  } else {
-
-    point_for_int = sf::st_as_sf(data.frame(lng = focus_wb_coordinates[1],
-                                            lat = focus_wb_coordinates[2]),
-                                 coords = c("lng","lat"),
-                                 crs = 4326) |>
-      sf::st_transform(crs = 3005)
-
-    focus_wb_poly = bcdata::bcdc_query_geodata('cb1e3aba-d3fe-4de1-a2d4-b8b6650fb1f6') |>
-      bcdata::filter(bcdata::INTERSECTS(point_for_int)) |>
-      bcdata::collect()
-  }
+  # # Make sure lake name is in title format.
+  # if(!is.null(focus_wb_name)){
+  # focus_wb_name = stringr::str_to_title(focus_wb_name)
+  # }
+  #
+  # # No coordinates?
+  # if(is.null(focus_wb_coordinates)){
+  #   # But there is a name?
+  #   if(!is.null(focus_wb_name)){
+  #     # Look for the lake by name.
+  #     lake_find_attempt = bcdata::bcdc_query_geodata('cb1e3aba-d3fe-4de1-a2d4-b8b6650fb1f6') |>
+  #       bcdata::filter(GNIS_NAME_1 == focus_wb_name) |>
+  #       bcdata::collect()
+  #     # Is there only a single lake in BC with that name?
+  #     if(nrow(lake_find_attempt) == 1){
+  #       # If so, use that!
+  #       focus_wb_poly = lake_find_attempt
+  #     } else {
+  #       focus_wb_poly = lake_find_attempt |>
+  #         dplyr::mutate(Area = 1e-6 * as.numeric(sf::st_area(geometry))) |>
+  #         dplyr::arrange(desc(AREA)) |> dplyr::slice(1)
+  #       cat(paste0("We searched for the lake with only a name, and ", nrow(lake_find_attempt), " lakes were returned."))
+  #       cat("Using the largest lake from that list...")
+  #     }
+  #   }
+  # } else {
+  #
+  #   point_for_int = sf::st_as_sf(data.frame(lng = focus_wb_coordinates[1],
+  #                                           lat = focus_wb_coordinates[2]),
+  #                                coords = c("lng","lat"),
+  #                                crs = 4326) |>
+  #     sf::st_transform(crs = 3005)
+  #
+  #   focus_wb_poly = bcdata::bcdc_query_geodata('cb1e3aba-d3fe-4de1-a2d4-b8b6650fb1f6') |>
+  #     bcdata::filter(bcdata::INTERSECTS(point_for_int)) |>
+  #     bcdata::collect()
+  # }
+  focus_wb_poly = get_waterbody_polygon(focus_wb_name,
+                        focus_wb_coordinates)
 
   if(in_shiny) shiny::incProgress(amount = 1/5, message = 'waterbody polygon downloaded.')
 
-  centroid_points = as.data.frame(st_centroid(focus_wb_poly) |> st_transform(crs = 4326) |> st_coordinates())
+  centroid_points = as.data.frame(sf::st_centroid(focus_wb_poly) |> sf::st_transform(crs = 4326) |> sf::st_coordinates())
 
   wb_data = get_relevant_data(focus_wb_poly, dist_from_wb_to_search)
 

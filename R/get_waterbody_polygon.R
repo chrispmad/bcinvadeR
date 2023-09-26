c4b1001b33d3ea14d5e4d7a8e11bbaf99321203e
Madsen <Chris.Madsen@gov.bc.ca>
2023-09-26 08:22:26 -0700

    massage input coords... again...

--- R/get_waterbody_polygon.R
+++ R/get_waterbody_polygon.R
@@ -12,7 +12,7 @@ get_waterbody_polygon = function(focus_wb_name = NULL,
 
   focus_wb_coordinates = c(unname(sapply(focus_wb_coordinates, as.numeric)))
 
-  if(focus_wb_coordinates[1] == "" & focus_wb_coordinates[2] == "") focus_wb_coordinates = c(NULL,NULL)
+  if(is.na(focus_wb_coordinates[1]) & is.na(focus_wb_coordinates[2])) focus_wb_coordinates = c(NULL,NULL)
 
   if(is.null(focus_wb_name) & is.null(focus_wb_coordinates)) stop("Please enter waterbody name or, preferably, coordinates.")
   if(!is.null(focus_wb_coordinates) & length(focus_wb_coordinates) != 2) stop("Coordinates must be in the format: c(longitude, latitude); remember the minus sign for longitude!")

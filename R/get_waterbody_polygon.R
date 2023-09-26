4727d4b24026db4a1ec9929945d686ba31be8c3c
Madsen <Chris.Madsen@gov.bc.ca>
2023-09-26 08:07:13 -0700

    add edge case where coords are ""

--- R/get_waterbody_polygon.R
+++ R/get_waterbody_polygon.R
@@ -10,6 +10,8 @@
 get_waterbody_polygon = function(focus_wb_name = NULL,
                                  focus_wb_coordinates = NULL){
 
+  if(focus_wb_coordinates[1] == "" & focus_wb_coordinates[2] == "") focus_wb_coordinates = c(NULL,NULL)
+
   if(is.null(focus_wb_name) & is.null(focus_wb_coordinates)) stop("Please enter waterbody name or, preferably, coordinates.")
   if(!is.null(focus_wb_coordinates) & length(focus_wb_coordinates) != 2) stop("Coordinates must be in the format: c(longitude, latitude); remember the minus sign for longitude!")
   # Make sure lake name is in title format.

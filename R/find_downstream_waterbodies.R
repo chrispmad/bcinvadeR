d5608c602ebe8a4c727cea99af1ce7855e9a28d1
Madsen <Chris.Madsen@gov.bc.ca>
2023-12-27 13:44:11 -0800

    unify naming convention between functions

--- R/find_downstream_waterbodies.R
+++ R/find_downstream_waterbodies.R
@@ -9,21 +9,22 @@
 
 #' Title Find the downstream waterbodies
 #'
-#' @param focus_wb_name Name of waterbody
-#' @param focus_wb_coordinates Coordinates within waterbody (long, lat)
+#' @param waterbody_name Name of waterbody
+#' @param waterbody_coordinates Coordinates within waterbody (long, lat)
 #' @param dist_from_wb_to_search Radius to search for streams/lakes, in kilometers
 #' @param in_shiny Is this function being run in shiny? If so, give incremental progress updates.
-
+#' @param the_session The shiny session
 #'
 #' @return sf object spatial table of downstream lakes
 #' @export
 #'
 #' @examples \dontrun
 find_downstream_waterbodies = function(
-    focus_wb_name = NULL,
-    focus_wb_coordinates = NULL,
+    waterbody_name = NULL,
+    waterbody_coordinates = NULL,
     dist_from_wb_to_search = 10, # This number is in kilometers.
-    in_shiny = F
+    in_shiny = F,
+    the_session = session
 ){
 
   # # # # # # # # # # # # # # # # #
@@ -31,17 +32,17 @@ find_downstream_waterbodies = function(
   # # # # # # # # # # # # # # # # #
 
   # # Make sure lake name is in title format.
-  # if(!is.null(focus_wb_name)){
-  # focus_wb_name = stringr::str_to_title(focus_wb_name)
+  # if(!is.null(waterbody_name)){
+  # waterbody_name = stringr::str_to_title(waterbody_name)
   # }
   #
   # # No coordinates?
-  # if(is.null(focus_wb_coordinates)){
+  # if(is.null(waterbody_coordinates)){
   #   # But there is a name?
-  #   if(!is.null(focus_wb_name)){
+  #   if(!is.null(waterbody_name)){
   #     # Look for the lake by name.
   #     lake_find_attempt = bcdata::bcdc_query_geodata('cb1e3aba-d3fe-4de1-a2d4-b8b6650fb1f6') |>
-  #       bcdata::filter(GNIS_NAME_1 == focus_wb_name) |>
+  #       bcdata::filter(GNIS_NAME_1 == waterbody_name) |>
   #       bcdata::collect()
   #     # Is there only a single lake in BC with that name?
   #     if(nrow(lake_find_attempt) == 1){
@@ -57,8 +58,8 @@ find_downstream_waterbodies = function(
   #   }
   # } else {
   #
-  #   point_for_int = sf::st_as_sf(data.frame(lng = focus_wb_coordinates[1],
-  #                                           lat = focus_wb_coordinates[2]),
+  #   point_for_int = sf::st_as_sf(data.frame(lng = waterbody_coordinates[1],
+  #                                           lat = waterbody_coordinates[2]),
   #                                coords = c("lng","lat"),
   #                                crs = 4326) |>
   #     sf::st_transform(crs = 3005)
@@ -67,12 +68,18 @@ find_downstream_waterbodies = function(
   #     bcdata::filter(bcdata::INTERSECTS(point_for_int)) |>
   #     bcdata::collect()
   # }
-  focus_wb_poly = get_waterbody_polygon(focus_wb_name,
-                        focus_wb_coordinates)
+
+  focus_wb_poly = get_waterbody_polygon(waterbody_name,
+                        waterbody_coordinates)
+
+  function_logic = function(){
 
   if(in_shiny) shiny::incProgress(amount = 1/5, message = 'waterbody polygon downloaded.')
 
   centroid_points = as.data.frame(sf::st_centroid(focus_wb_poly) |> sf::st_transform(crs = 4326) |> sf::st_coordinates())
+
+  # If no waterbody name was fed in to function, grab it from the waterbody polygon we downloaded.
+  if(is.null(waterbody_name)) waterbody_name = focus_wb_poly$GNIS_NAME_1
 
   wb_data = get_relevant_data(focus_wb_poly, dist_from_wb_to_search)
 
@@ -88,7 +95,11 @@ find_downstream_waterbodies = function(
 
   if(in_shiny) shiny::incProgress(amount = 1/5, message = 'flow lines extended...')
 
-  downstream_stream_network = find_downstream_graph(focus_wb)
+  downstream_stream_network = find_downstream_graph(waterbody_name,
+                                                    focus_wb_poly,
+                                                    lakes,
+                                                    streams,
+                                                    flow_lines)
 
   if(in_shiny) shiny::incProgress(amount = 1/5, message = 'network composition complete!')
 
@@ -98,6 +109,18 @@ find_downstream_waterbodies = function(
   if(in_shiny) shiny::incProgress(amount = 1/4, message = 'lakes in network chosen.')
 
   return(downstream_lakes)
+  }
+
+  if(in_shiny){
+    wbs = shiny::withProgress(
+      message = 'Finding Connected Waterbodies',
+      session = the_session,
+      expr = function_logic()
+    )
+  } else {
+    wbs = function_logic()
+  }
+  wbs
 }
 #
 #   downstream_lakes |>
